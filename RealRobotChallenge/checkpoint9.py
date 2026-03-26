e45def6bd1b7bd8bfdf3913560a626d96cac3b19
OPYGOO <131343400+OPYGOO@users.noreply.github.com>
2026-03-26 02:00:42 -0500

    Checkpoint9_initial add
    Added functionality to detect and grasp red and green cubes, including visualization of cube poses.

--- RealRobotChallenge/checkpoint9.py
+++ RealRobotChallenge/checkpoint9.py
@@ -1,10 +1,11 @@
 from checkpoint8 import CubePoseDetector
 
-import cv2, time
+import cv2, numpy, time
 from xarm.wrapper import XArmAPI
 
 from utils.vis_utils import draw_pose_axes
 from utils.zed_camera import ZedCamera
+from checkpoint0 import get_transform_camera_robot
 from checkpoint1 import grasp_cube, place_cube, GRIPPER_LENGTH
 from checkpoint4 import STACK_HEIGHT
 
@@ -34,8 +35,47 @@ def main():
         cv_image = zed.image
         point_cloud = zed.point_cloud
 
-        # TODO
-    
+        # Get camera-to-robot transformation
+        t_cam_robot = get_transform_camera_robot(cv_image, camera_intrinsic)
+        if t_cam_robot is None:
+            return
+        cube_pose_detector.set_camera_pose(t_cam_robot)
+
+        # Detect red cube (to pick up)
+        result_red = cube_pose_detector.get_transforms([cv_image, point_cloud], 'red cube')
+        if result_red is None:
+            print('Red cube not found.')
+            return
+        t_robot_red, t_cam_red = result_red
+
+        # Detect green cube (stack target)
+        result_green = cube_pose_detector.get_transforms([cv_image, point_cloud], 'green cube')
+        if result_green is None:
+            print('Green cube not found.')
+            return
+        t_robot_green, t_cam_green = result_green
+
+        # Visualization
+        draw_pose_axes(cv_image, camera_intrinsic, t_cam_red)
+        draw_pose_axes(cv_image, camera_intrinsic, t_cam_green)
+        cv2.namedWindow('Verifying Cube Poses', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Verifying Cube Poses', 1280, 720)
+        cv2.imshow('Verifying Cube Poses', cv_image)
+        key = cv2.waitKey(0)
+
+        if key == ord('k'):
+            cv2.destroyAllWindows()
+
+            # Grasp the red cube
+            grasp_cube(arm, t_robot_red)
+
+            # Compute stacking pose: green cube position + stack height offset
+            t_stack = t_robot_green.copy()
+            t_stack[2, 3] += STACK_HEIGHT
+
+            # Place the red cube on top of the green cube
+            place_cube(arm, t_stack)
+
     finally:
         # Close Lite6 Robot
         arm.move_gohome(wait=True)
