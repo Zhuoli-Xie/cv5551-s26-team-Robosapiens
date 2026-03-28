5735d46450e24834cdb60878dc95c15fae3f0ba1
OPYGOO <131343400+OPYGOO@users.noreply.github.com>
2026-03-28 12:42:55 -0500

    Implement cube detection and stacking logic
    Added camera-to-robot transformation and cube detection logic.

--- RealRobotChallenge/checkpoint5.py
+++ RealRobotChallenge/checkpoint5.py
@@ -1,15 +1,16 @@
 from checkpoint3 import CubePoseDetector
 
-import cv2, time
+import cv2, numpy, time
 from xarm.wrapper import XArmAPI
 
 from utils.vis_utils import draw_pose_axes
 from utils.zed_camera import ZedCamera
+from checkpoint0 import get_transform_camera_robot
 from checkpoint1 import grasp_cube, place_cube, GRIPPER_LENGTH
 from checkpoint4 import STACK_HEIGHT
 
 stacking_order = ['red cube', 'green cube', 'blue cube']   # From top to bottom
-robot_ip = ''
+robot_ip = '192.168.1.183'
 
 def main():
 
@@ -34,8 +35,59 @@ def main():
         # Get Observation
         cv_image = zed.image
 
-        # TODO
-    
+        # Get camera-to-robot transformation
+        t_cam_robot = get_transform_camera_robot(cv_image, camera_intrinsic)
+        if t_cam_robot is None:
+            return
+        cube_pose_detector.set_camera_pose(t_cam_robot)
+
+        # Detect all three cubes
+        cube_poses = {}
+        for cube_name in stacking_order:
+            result = cube_pose_detector.get_transforms(cv_image, cube_name)
+            if result is None:
+                print(f'{cube_name} not found.')
+                return
+            cube_poses[cube_name] = result
+
+        # Visualization - show all cubes
+        for cube_name in stacking_order:
+            _, t_cam_cube = cube_poses[cube_name]
+            draw_pose_axes(cv_image, camera_intrinsic, t_cam_cube)
+        cv2.namedWindow('Verifying Cube Poses', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Verifying Cube Poses', 1280, 720)
+        cv2.imshow('Verifying Cube Poses', cv_image)
+        key = cv2.waitKey(0)
+
+        if key == ord('k'):
+            cv2.destroyAllWindows()
+
+            # Stacking order is top to bottom: [top, middle, bottom]
+            # Build order is reversed: place bottom first, then middle on bottom, then top on middle
+            # Step 1: Stack middle cube onto bottom cube
+            bottom_cube = stacking_order[2]
+            middle_cube = stacking_order[1]
+            top_cube = stacking_order[0]
+
+            # Pick middle cube and place on bottom cube
+            t_robot_middle, _ = cube_poses[middle_cube]
+            t_robot_bottom, _ = cube_poses[bottom_cube]
+
+            grasp_cube(arm, t_robot_middle)
+
+            t_stack = t_robot_bottom.copy()
+            t_stack[2, 3] += STACK_HEIGHT
+            place_cube(arm, t_stack)
+
+            # Step 2: Pick top cube and place on the stack (now 2 cubes high)
+            t_robot_top, _ = cube_poses[top_cube]
+
+            grasp_cube(arm, t_robot_top)
+
+            t_stack2 = t_robot_bottom.copy()
+            t_stack2[2, 3] += STACK_HEIGHT * 2
+            place_cube(arm, t_stack2)
+
     finally:
         # Close Lite6 Robot
         arm.move_gohome(wait=True)
